50c4f8d7133ce680da03a27c172736c1d8cbffd6
XiaoNing <xiaoning@genomics.cn>
2021-11-10 10:30:34 +0800

    Vue脚手架的初体验

---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="student">
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '张三',
+                age: 20
+            }
+        }
+    }
+</script>
+
+<style>
+    .student {
+        width: 50px;
+        height: 50px;
+        background-color: orange;
+    }
+</style>
