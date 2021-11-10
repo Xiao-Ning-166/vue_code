ce882de8b0ac2b06eebe3c6d632cdb14a2f0be0a
XiaoNing <475529787@qq.com>
2021-11-10 22:25:37 +0800

    TodoList案例

---
+++ todo_list_demo/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <ListHeader></ListHeader>
+    <ListBody></ListBody>
+    <ListFooter></ListFooter>
+  </div>
+</template>
+
+<script>
+  // 引入组件
+  import ListHeader from './components/ListHeader.vue'
+  import ListBody from './components/ListBody.vue'
+  import ListFooter from './components/ListFooter.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      ListHeader,
+      ListBody,
+      ListFooter
+    }
+  }
+
+</script>
+
+<style>
+  #app {
+    background-color: #fff;
+    width: 600px;
+    margin: 0 auto;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+  .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+  }
+
+  .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+  }
+
+  .btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+  }
+
+  .btn:focus {
+    outline: none;
+  }
+</style>
