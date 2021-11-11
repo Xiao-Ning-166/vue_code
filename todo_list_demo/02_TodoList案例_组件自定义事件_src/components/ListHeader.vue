72e6222a193163b43326e454ef36b3e266718b81
XiaoNing <475529787@qq.com>
2021-11-11 21:04:42 +0800

    TodoList案例 —— 组件自定义事件

---
+++ todo_list_demo/02_TodoList案例_组件自定义事件_src/components/ListHeader.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="list-header">
+        <input type="text" @keyup.enter="add" v-model="name" placeholder="请输入你的任务名称，按回车键确认"/>
+    </div>
+</template>
+
+<script>
+    import {nanoid} from 'nanoid'
+    export default {
+        name: 'ListHeader',
+        data() {
+            return {
+                name: '',
+                todo: {}
+            }
+        },
+        methods: {
+            add() {
+                if(!this.name.trim()) {
+                    return
+                }
+                // 封装todo对象
+                this.todo = {id:nanoid(),name:this.name,isFinish:false}
+                // 触发addTodo事件
+                this.$emit('addTodo',this.todo)
+                // 重置输入框
+                this.name = ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .list-header input {
+        width: 560px;
+        height: 28px;
+        font-size: 14px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 4px 7px;
+    }
+
+    .list-header input:focus {
+        outline: none;
+        border-color: rgba(82, 168, 236, 0.8);
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+    }
+</style>
---
+++ todo_list_demo/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pages: {
+      index: {
+        // 配置App的入口程序
+        // 01_TodoList案例_原生
+        // entry: '01_TodoList案例_原生_src/main.js'
+
+        // 02_TodoList案例_组件自定义事件
+        entry: '02_TodoList案例_组件自定义事件_src/main.js'
+      } 
+    }
+  }
