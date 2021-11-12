4da5d452f35abc795872adaa6fd800fba598a20b
XiaoNing <xiaoning@genomics.cn>
2021-11-12 16:00:48 +0800

    vue解决开发中的跨域问题

--- README.md
+++ README.md
@@ -165,3 +165,46 @@
         </transition>
         ```
     3. 备注：若有多个元素需要过渡，则需要使用 `<transition-group>` ，且每个元素都要指定 `key` 值
+
+    <br><br>
+
+## vue脚手架配置代理
+### 方法一
+在 `vue.config.js` 中添加如下配置：<br>
+```javascritp
+devServer: {
+    proxy: "目标地址"
+    // 例如：proxy: "http://localhost:5000"
+}
+```
+
+#### 说明
+1. 优点：配置简单，请求资源时直接发给前端（8080）即可
+2. 缺点：不能配置多个代理，不能灵活控制请求是否走代理
+3. 工作方式：若按照上述配置代理，当请求了前端不存在的资源时，那么该请求会转发给服务器（优先匹配前端资源）
+
+### 方法二
+编写 `vue.config.js` 配置具体代理规则：
+```javascript
+  devServer: {
+    proxy: {
+        // 匹配所有以 http://ip:port/'atguigu' 开头的请求路径
+        '/atguigu': {
+            // 代理目标的基础路径
+            target: 'http://localhost:5000',
+            // 地址重写，去掉请求路径中的 /atguigu
+            pathRewrite: {'^/atguigu':''},
+            // 用于支持 websocket
+            ws: true,
+            // 用于控制请求头中的host值。默认为true
+            // true：服务器收到的请求头中的host为：localhost:5000
+            // false：服务器收到的请求头中的host为：localhost:8080
+            changeOrigin: true
+        }
+    }
+  }
+```
+
+#### 说明
+1. 优点：可以配置多个代理，且可以灵活的控制请求是否走代理
+2. 缺点：配置略微繁琐，请求资源时必须加前缀
---
+++ vue_test/11_vue中的axios操作_src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="app">
+        <h1 v-text="title"></h1>
+        
+        <button @click="getStudentMsg">获取学生信息</button>
+    </div>
+</template>
+
+<script>
+    // 引入axios
+    import axios from 'axios'
+    export default {
+        name: 'App',
+        components: {
+
+        },
+        data() {
+            return {
+                title: 'Vue中axios的使用'
+            }
+        },
+        methods: {
+            getStudentMsg() {
+                /*
+                // 方式一
+                axios.get('http://localhost:8080/students').then(
+                    reponse => {
+                        console.log('请求成功了',reponse.data)
+                    },
+                    error => {
+                        console.log('请求失败了',error.message)
+                    }
+                )
+                */
+               // 方式二
+               axios.get('http://localhost:8080/atguigu/students').then(
+                   reponse => {
+                        console.log('请求成功了',reponse.data)
+                    },
+                    error => {
+                        console.log('请求失败了',error.message)
+                    }
+               )
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
